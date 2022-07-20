023be6d96d660dffa1e0f13fca9711fd2d536628
Jay Krishna Reddy <jaykrishnareddy@gmail.com>
2022-07-20 16:03:02 +0530

    OpenAPIOperation Changes

--- BasicAuth_AzureFunction_API/Function1.cs
+++ BasicAuth_AzureFunction_API/Function1.cs
@@ -1,81 +1,87 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Newtonsoft.Json;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+    using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.OpenApi.Models;
+    using Newtonsoft.Json;
 
-namespace BasicAuth_AzureFunction_API
-{
-    public class Function1
+    namespace BasicAuth_AzureFunction_API
     {
-        private readonly ILogger<Function1> _logger;
-
-        public Function1(ILogger<Function1> log)
+        public class Function1
         {
-            _logger = log;
-        }
+            private readonly ILogger<Function1> _logger;
 
-        [FunctionName("Function1")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
-        [OpenApiSecurity("basic_auth", SecuritySchemeType.Http, Scheme =OpenApiSecuritySchemeType.Basic)]
-        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
-        {
-            var headers = req.Headers["Authorization"];
-            if (ValidateToken(headers))
+            public Function1(ILogger<Function1> log)
             {
-                _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-               string name = req.Query["name"];
-
-               string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-               dynamic data = JsonConvert.DeserializeObject(requestBody);
-               name = name ?? data?.name;
-                string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-                return new OkObjectResult(responseMessage);
+                _logger = log;
             }
-            else
+
+            [FunctionName("Function1")]
+            [OpenApiOperation(operationId: "Run", tags: new[] { "http" }, Summary = "Basic authentication token flow via header", Description = "This shows the basic authentication token flow via header", Visibility = OpenApiVisibilityType.Important)]
+            [OpenApiSecurity("basic_auth", SecuritySchemeType.Http, Scheme = OpenApiSecuritySchemeType.Basic)]
+            [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+            [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+            public async Task<IActionResult> Run(
+                [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
             {
-                return new UnauthorizedResult();
-            }
+                 //Getting the Header - extracting from the request
+                var headers = req.Headers["Authorization"];
+                if (ValidateToken(headers))
+                {
+                    _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+                   string name = req.Query["name"];
+
+                   string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                   dynamic data = JsonConvert.DeserializeObject(requestBody);
+                   name = name ?? data?.name;
+                    string responseMessage = string.IsNullOrEmpty(name)
+                    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+                    return new OkObjectResult(responseMessage);
+                }
+                else
+                {
+                    return new UnauthorizedResult();
+                }
             
-        }
-
-        private bool ValidateToken(string header)
-        {
-            if (!string.IsNullOrEmpty(header) && header.StartsWith("Basic"))
-            {
-                string encodedUsernamePassword = header.Substring("Basic ".Length).Trim();
-                Encoding encoding = Encoding.GetEncoding("iso-8859-1");
-                string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
-
-                int seperatorIndex = usernamePassword.IndexOf(':');
-
-                var username = usernamePassword.Substring(0, seperatorIndex);
-                var password = usernamePassword.Substring(seperatorIndex + 1);
-                if (username is "Jay" && password is "12345") return true;
-                else return false;
             }
-            else
+
+            private bool ValidateToken(string header)
             {
-                return false;
+                //Checking the header
+                if (!string.IsNullOrEmpty(header) && header.StartsWith("Basic"))
+                {
+                    //Extracting credentials
+                    // Removing "Basic " Substring
+                    string encodedUsernamePassword = header.Substring("Basic ".Length).Trim();
+                    //Decoding Base64
+                    Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+                    string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+                    //Splitting Username:Password
+                    int seperatorIndex = usernamePassword.IndexOf(':');
+                    // Extracting the individual username and password
+                    var username = usernamePassword.Substring(0, seperatorIndex);
+                    var password = usernamePassword.Substring(seperatorIndex + 1);
+                    //Validating the credentials 
+                    if (username is "Jay" && password is "12345") return true;
+                    else return false;
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
     }
-}
 
