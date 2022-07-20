8446df19f98cdda5aa08946bac2c9cacd41bb97c
Jay Krishna Reddy <jaykrishnareddy@gmail.com>
2022-07-20 13:57:27 +0530

    Final updates.

--- BasicAuth_AzureFunction_API/Function1.cs
+++ BasicAuth_AzureFunction_API/Function1.cs
@@ -33,16 +33,16 @@ namespace BasicAuth_AzureFunction_API
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
             var headers = req.Headers["Authorization"];
             if (ValidateToken(headers))
             {
+                _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+               string name = req.Query["name"];
+
+               string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+               dynamic data = JsonConvert.DeserializeObject(requestBody);
+               name = name ?? data?.name;
                 string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
